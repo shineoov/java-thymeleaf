890edfd0de655104623a1a8c73a276d387e4c987
shineoov <shineoov88@gmail.com>
2022-01-29 00:59:21 +0900

    project init

---
+++ src/test/java/shineoov/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package shineoov.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
