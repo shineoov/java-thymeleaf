57b4081a87984c144406cacc406443abf7153dea
shineoov <shineoov88@gmail.com>
2022-01-30 01:37:54 +0900

    Thymeleaf - 기본 변수 사용

--- src/main/java/shineoov/thymeleaf/BasicController.java
+++ src/main/java/shineoov/thymeleaf/BasicController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shineoov.thymeleaf.domain.User;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -18,8 +24,25 @@ public class BasicController {
     @GetMapping("/text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "<b>Java!!</b>");
-
         return "basic/text-unescaped";
     }
 
+    @GetMapping("/variable")
+    public String valiable(Model model) {
+
+        User userA = new User("Apple", 10);
+        User userB = new User("Banana", 20);
+
+        List<User> userList = Arrays.asList(userA, userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("userList", userList);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/variable";
+    }
 }
---
+++ src/main/java/shineoov/thymeleaf/domain/User.java
@@ -0,0 +1,15 @@
+package shineoov.thymeleaf.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+    private int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
