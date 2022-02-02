095cd8442f44120a76efdae121cf97ada06ac5f9
shineoov <shineoov88@gmail.com>
2022-02-03 00:04:11 +0900

    Thymeleaf - HTML 태그 속성

--- src/main/java/shineoov/thymeleaf/BasicController.java
+++ src/main/java/shineoov/thymeleaf/BasicController.java
@@ -88,4 +88,9 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(Model model) {
+
+        return "basic/attribute";
+    }
 }
